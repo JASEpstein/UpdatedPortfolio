b99d2863c5031ebcbb61c84afc2d5e2f46683bb9
JASEpstein <jasepstein@gmail.com>
2019-03-23 17:57:19 -0500

    Header mockup + WIP carousel for portfolio cards

--- portfolio.js
+++ portfolio.js
@@ -11,5 +11,31 @@ $('.hero-welcome')
         animation: 'scale in',
         duration: '2s'
     });
+
+$('.hero-welcome2').delay(900)
+    .transition({
+        animation: 'scale in',
+        duration: '2s',
+        delay: 9000
+    });
+
+$(document).ready(function () {
+    $('.portfolio-carousel').slick({
+
+    });
+});
+
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+$('.special.cards .image').dimmer({
+    on: 'hover'
+});
+
+
 // $(document).on('ready', function () {
 // });
