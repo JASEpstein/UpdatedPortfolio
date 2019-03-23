4d579dda7e3add50d54f6bf633ee7195c047edec
JASEpstein <jasepstein@gmail.com>
2019-03-22 23:41:59 -0500

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+$('.ui.sticky')
+    .sticky({
+        context: '#example1'
+    });
+
+$('.ui.dropdown')
+    .dropdown();
+
+$('.hero-welcome')
+    .transition({
+        animation: 'scale in',
+        duration: '2s'
+    });
+// $(document).on('ready', function () {
+// });
